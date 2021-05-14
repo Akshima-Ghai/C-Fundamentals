9577cbc65524fb0301e5190e118a5c191690f0a7
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2021-05-14 13:54:34 +0530

    Add files via upload

---
+++ binary_to_decimal.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string n;
+    cin >> n;
+    int ans = 0;
+    int base = 1;
+    int num = n.length() - 1;
+    for (int i = num; i >= 0; i--)
+    {
+        ans += (n[i] * base);
+        base = base * 2;
+    }
+    cout << ans;
+    return 0;
+}
