ee8bf6335e8919621cbe7db9c35c3af7ae2e5bfc
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2021-05-17 13:42:18 +0530

    Update max_struct.cpp

--- max_struct.cpp
+++ max_struct.cpp
@@ -6,3 +6,27 @@ struct Height {
 	int feet;
 	int inches;
 };
+
+int findMax(Height arr[], int n);
+
+// driver program
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n, tmp1, tmp2;
+	    cin>>n;
+	    Height arr[n];
+	    for(int i=0; i<n; i++)
+	    {
+	        cin>>tmp1>>tmp2;
+	        arr[i].feet = tmp1;
+	        arr[i].inches = tmp2;
+	    }
+	    int res = findMax(arr, n);
+	    cout<<res<<endl;
+	}
+	return 0;
+}// } Driver Code Ends
