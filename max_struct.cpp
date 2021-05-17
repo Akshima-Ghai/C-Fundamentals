3af8b12053a8504a6d0297d39ac62ef16b740859
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2021-05-17 13:44:00 +0530

    Update max_struct.cpp

--- max_struct.cpp
+++ max_struct.cpp
@@ -30,3 +30,25 @@ int main()
 	}
 	return 0;
 }// } Driver Code Ends
+
+
+/*
+Structure of the element of the array is as
+struct Height {
+	int feet;
+	int inches;
+};
+*/
+// function must return the maximum Height
+// return the height in inches
+int findMax(Height arr[], int n)
+{
+    int mx = INT_MIN;
+    for(int i=0;i<n;i++){
+        int temp = 12 * (arr[i].feet) 
+                     + arr[i].inches;
+        mx = max(mx,temp);
+    }
+    // Code here
+    return mx;
+}
