742e013ba612d8db96d39c04dd44e1a74e8923a3
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2021-06-11 16:58:29 +0530

    Create binarySearch.cpp

---
+++ binarySearch.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int ele)
+{
+    int start = 0, end = n - 1;
+    while (start < end)
+    {
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == ele)
+        {
+            return mid;
+        }
+        else if (ele < arr[mid])
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int ele;
+    cin >> ele;
+    cout << binarySearch(arr, n, ele);
+
+    return 0;
+}
