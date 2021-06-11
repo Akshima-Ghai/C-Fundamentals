59efec1fab1b4cf71505febc790cd489bd00ccab
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2021-06-11 22:15:07 +0530

    Create lastOccurenceBinarySearch.cpp

---
+++ lastOccurenceBinarySearch.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int reverseBinarySearch(int arr[], int n, int ele)
+{
+    int start = 0, end = n - 1, res;
+    while (start < end)
+    {
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == ele)
+        {
+            res = mid;
+            start = mid + 1;
+        }
+        else if (arr[mid] < ele)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+    return res;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int ele;
+    cin >> ele;
+    cout << reverseBinarySearch(arr, n, ele);
+    return 0;
+}
