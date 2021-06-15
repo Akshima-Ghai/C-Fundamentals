a4c726a3f1380bd010c1359d489bad43e8e317d9
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2021-06-16 03:21:01 +0530

    Update reverseString.cpp

--- reverseString.cpp
+++ reverseString.cpp
@@ -1,16 +1,35 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     string s = "python";
+//     char a[7];
+//     int j = 0;
+//     for (int i = 5; i >= 0; i--)
+//     {
+//         a[j] = s[i];
+//         j++;
+//     }
+//     a[j] = '\0';
+//     cout << a;
+
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 int main()
 {
     string s = "python";
-    char a[7];
-    int j = 0;
-    for (int i = 5; i >= 0; i--)
+    char t;
+    int j = 5;
+    for (int i = 0; i < j; i++)
     {
-        a[j] = s[i];
-        j++;
+        t = s[i];
+        s[i] = s[j];
+        s[j] = t;
+        j--;
     }
-    cout << a;
-
+    cout << s;
     return 0;
 }
