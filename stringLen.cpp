825da496fa823e7ca51ed3d72e35eae10872f965
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2021-06-16 00:08:36 +0530

    Create stringLen.cpp

---
+++ stringLen.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    int i;
+    for (i = 0; s[i] != '\0'; i++)
+    {
+    }
+    cout << i;
+    return 0;
+}
