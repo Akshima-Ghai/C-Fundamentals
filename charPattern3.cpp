cb3166b6c0b211b1fc96d3bb0e2285dbdf285f74
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2021-06-18 13:49:24 +0530

    Create charPattern3.cpp

---
+++ charPattern3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 65; i < 65 + n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << char(i + j);
+        }
+        cout << endl;
+    }
+    return 0;
+}
